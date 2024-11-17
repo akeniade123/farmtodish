22c797d28f5e09d9f4a4859e82c4e13e755202c8
akeniade <adeyinkaakeni@gmail.com>
2024-11-17 15:53:07 +0100

    table integrate

--- lib/global_string.dart
+++ lib/global_string.dart
@@ -11,3 +11,29 @@ const String token = "token";
 const String prlmtpc = "tpks";
 const String defdm = "defaultdomain";
 String paystruct = "Paystruct";
+
+const String ptyp = "product_type";
+const String appl = "applicants";
+//  const String ctg  = 
+
+
+
+/*
+category
+department
+gender
+hub
+lga
+order
+order_item
+order_type
+price_index
+produce
+produce_type
+section
+states
+unit
+units
+users
+vendor
+*/
