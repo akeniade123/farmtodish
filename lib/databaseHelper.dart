5d0353a2f89a59e9b053a87e0c2d52b82d36172b
akeniade <adeyinkaakeni@gmail.com>
2024-11-18 09:59:42 +0100

    dbhelper

--- lib/databaseHelper.dart
+++ lib/databaseHelper.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:developer';
 
 import 'package:path_provider/path_provider.dart';
@@ -81,5 +82,75 @@ class DatabaseHelper {
         }
       },
     );
+  }
+
+  Future<List<Map<String, dynamic>>> queryAllRows() async {
+    await init();
+    return await _db.query(table);
+  }
+
+  // All of the methods (insert, query, update, delete) can also be done using
+  // raw SQL commands. This method uses a raw query to give the row count.
+  Future<int> queryRowCount() async {
+    await init();
+    final results = await _db.rawQuery('SELECT COUNT(*) FROM $table');
+    return Sqflite.firstIntValue(results) ?? 0;
+  }
+
+  Future<bool> rowExists(Map<String, dynamic> fields) async {
+    await init();
+
+    Map<String, dynamic> whr = whereClause(fields);
+    List jjk = await _db.query(table,
+        where: whr["constraint"], whereArgs: whr["entries"]);
+    if (jjk.isNotEmpty) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // We are assuming here that the id column in the map is set. The other
+  // column values will be used to update the row.
+  Future<int> update(Map<String, dynamic> row, String key, String value) async {
+    return await _db.update(
+      table,
+      row,
+      where: '$key = ?',
+      whereArgs: [value],
+    );
+  }
+
+  // Deletes the row specified by the id. The number of affected rows is
+  // returned. This should be 1 as long as the row exists.
+  Future<int> delete(Map<String, dynamic> fields) async {
+    await init();
+    Map<String, dynamic> whr = whereClause(fields);
+    return await _db.delete(
+      table,
+      where: whr["constraint"],
+      whereArgs: whr["entries"],
+    );
+  }
+
+  Map<String, dynamic> whereClause(Map<String, dynamic> fields) {
+    String keys = "";
+    List values = [];
+    int p = 0;
+    int c = fields.length;
+    fields.forEach((key, value) {
+      keys += "$key = ? ";
+      if (c > p + 1) {
+        keys += " AND ";
+        p++;
+        values.add(value);
+      } else {
+        values.add(value);
+      }
+    });
+    Map<String, dynamic> whr = {"constraint": keys, "entries": values};
+    log(jsonEncode(whr));
+
+    return whr;
   }
 }
