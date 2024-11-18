ef637288520fe5b4a6174683fc1d72dbdb9c7932
akeniade <adeyinkaakeni@gmail.com>
2024-11-18 07:40:28 +0100

    table

--- lib/global_string.dart
+++ lib/global_string.dart
@@ -12,12 +12,22 @@ const String prlmtpc = "tpks";
 const String defdm = "defaultdomain";
 String paystruct = "Paystruct";
 
+const String hub = "hub";
+const String lga = "lga";
+const String order = "order";
+const String orderItem = "order_item";
+const String orderType = "order_type";
+const String priceIndex = "price_index";
+const String produce = "produce";
 const String ptyp = "product_type";
-const String appl = "applicants";
+const String section = "section";
+const String states = "states";
+const String unit = "unit";
+const String users = "users";
+const String vendor = "vendor";
+
+
 //  const String ctg  = 
-
-
-
 /*
 category
 department
